e1590b99fbd143145655c9ef6c40273c9ede56c5
suryaprakash-s-2005 <23cseb57.suryaprakash.s@gmail.com>
2026-01-23 20:41:07 +0530

    api routes configured

--- pixelfeed-frontend/src/api/axios.js
+++ pixelfeed-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pixelfeed-backend-api.onrender.com",
+  baseURL: "https://pixelfeed-backend-api.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
